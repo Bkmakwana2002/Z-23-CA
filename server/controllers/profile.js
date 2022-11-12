5ffc90b1ade68682e76c4e3b712508adbdaa4d90
Bkmakwana2002 <2020meb1278@iirpr.ac.in>
2022-11-13 04:11:54 +0530

    added api

---
+++ server/controllers/profile.js
@@ -0,0 +1,58 @@
+const firebase = require("./../config/firebase")
+const admin = require("../config/admin");
+const db = admin.firestore();
+const shortid = require("shortid");
+
+exports.addUserDetail = (req, res) => {
+    try {
+        const id = req.body.email;
+        const userJson = {
+            name: req.body.name,
+            email: req.body.email,
+            pic: req.body.pic,
+            idCard: req.body.idCard,
+            dob: req.body.dob,
+            YearOfPassing: req.body.YearOfPassing,
+            gender: req.body.gender,
+            phone: req.body.phone,
+            collegeName: req.body.collegeName,
+            collegeState: req.body.collegeState,
+            referral_code: "CA-" + shortid.generate(),
+            invite: 0,
+        };
+        const response = db.collection("CA").doc(id).set(userJson);
+        console.log("updated successfully");
+        res.json(response);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+exports.getUserDetail = async (req, res) => {
+    try {
+        const snapshot = await db.collection("CA").get()
+        const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        var temp
+        list.forEach(element => {
+            if (element.email === req.body.id) {
+                temp = element
+                res.status(200).json(element)
+            }
+        })
+        if (temp === undefined || temp === null) { res.status(400).json("not found") }
+    } catch (error) {
+        console.log("error");
+        res.status(500).send(error);
+    }
+}
+
+exports.leaderBoard = async (req, res) => {
+    try {
+        const snapshot = await db.collection("CA").get();
+        const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        res.status(200).send(list);
+    } catch (error) {
+        console.log("error");
+        res.status(500).send(error);
+    }
+}
---
+++ server/routes/verification.js
@@ -0,0 +1,22 @@
+const firebase = require("./../config/firebase");
+const admin = require("./../config/admin");
+
+
+module.exports = (req, res, next) => {
+const token = req.header('Authorization').replace('Bearer', '').trim()
+var user = firebase.auth().currentUser;
+if (user) {
+   admin.auth().verifyIdToken(token)
+  .then(function (decodedToken) {
+      if(decodedToken.uid === user.uid)
+      {
+         req.user = user.uid
+         return next()
+      }
+   }).catch(function (error) {
+     res.send.json({message: error});
+   });
+} else {
+   console.log("There is no current user.");
+}
+};
