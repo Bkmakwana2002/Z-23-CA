4d8ab46efc608110bc9d576b0495f6276d9c6fd3
dishu001 <2020eeb1167@iitrpr.ac.in>
2022-12-29 19:21:07 +0530

    remove extra toast

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -73,18 +73,6 @@ function Login() {
   }
   return (
     <>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
       <div className="form-container">
         {(() => {
           if (loading) {
--- client/src/components/Login/loginFirst.js
+++ client/src/components/Login/loginFirst.js
@@ -74,18 +74,6 @@ function LoginFirst() {
   };
   return (
     <>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
       {(() => {
         if (loading) {
           return <PreLoader />;
