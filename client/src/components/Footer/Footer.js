532d27f67695a9e2ae67c48beb7ee631ff0dc866
Bkmakwana2002 <2020meb1278@iirpr.ac.in>
2022-12-30 02:16:08 +0530

    changes in footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,72 +1,23 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiOutlineTwitter,
-  AiFillInstagram,
-  AiFillFacebook,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-import "./footer-style.css";
-
-function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+import React from 'react'
+import './footer-style.css'
+const Footer = () => {
   return (
-    <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Zeitgeist 2023</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Zeitgeist</h3>
-        </Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://twitter.com/zeitgeist_rpr"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://in.linkedin.com/in/zeitgeist-iit-ropar-aa2bb6166"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/zeitgeist_iitrpr/?hl=en"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.facebook.com/zeitgeist.iitrpr/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillFacebook />
-              </a>
-            </li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
-  );
+    <>
+      <footer>
+        <div className="myfooter">
+          <div className="myrow">
+            <a href="https://www.facebook.com/zeitgeist.iitrpr/"><i className="fa fa-facebook"></i></a>
+            <a href="https://www.instagram.com/zeitgeist_iitrpr/?hl=en"><i className="fa fa-instagram"></i></a>
+            <a href="https://in.linkedin.com/in/zeitgeist-iit-ropar-aa2bb6166"><i className="fa fa-linkedin"></i></a>
+            <a href="https://twitter.com/zeitgeist_rpr"><i className="fa fa-twitter"></i></a>
+          </div>
+          <div className="myrow">
+            Copyright © 2023 Zeitgeist - Made in collaboration with &nbsp; <a href = 'https://webguruz.in/'>WebGuruz</a>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
 }
 
-export default Footer;
+export default Footer
